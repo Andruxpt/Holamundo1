0980e68e8a912740003e19405d7860ed48fd3815
andruxp <andruxhill@gmail.com>
2024-06-21 20:34:20 -0600

    Se integro la clase Mensaje

--- src/com/acme/Holamundo.java
+++ src/com/acme/Holamundo.java
@@ -2,6 +2,8 @@ package com.acme;
 
 public class Holamundo {
     public static void main(String[] args) {
-        System.out.println("hola mundo!!");
+       Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Escribe tu nombre: ");
+        msg.Saludar(nombre);
     }
 }
